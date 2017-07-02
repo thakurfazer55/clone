ba044f9d50cae7f3ab97901192d5ad0b3cf651e1
thakurfazer55 <thakurfazer55@gmail.com>
2017-07-02 15:12:17 +0000

    Make cloning easy

---
+++ clone.sh
@@ -0,0 +1,5 @@
+git clone https://github.com/thakurfazer55/msm8916-common -b cm-14.1 device/cyanogen/msm8916-common
+git clone https://github.com/thakurfazer55/vendor_volte -b cm-14.1 vendor/volte
+git clone https://github.com/thakurfazer55/device_tomato -b cm-14.1 device/yu/tomato
+git clone https://github.com/thakurfazer55/vendor_yu -b cm-14.1 vendor/yu
+git clone https://github.com/Shoaib0597/Velocity_MSM8916 -b N-Tomato-MV kernel/cyanogen/msm8916
