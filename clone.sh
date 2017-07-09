f8137635003fb4c33f9a5c7d1c3f5e6a1bf10b96
Neeraj55#5 <thakurfazer55@gmail.com>
2017-07-09 23:32:34 +0530

    Fix SDClang

--- clone.sh
+++ clone.sh
@@ -3,5 +3,5 @@ git clone https://github.com/thakurfazer55/device_tomato -b cm-14.1 device/yu/to
 git clone https://github.com/thakurfazer55/vendor_yu -b cm-14.1 vendor/yu
 rm -rf prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9
 git clone https://bitbucket.org/matthewdalex/aarch64-linux-android-4.9 prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9
-git clone https://bitbucket.org/matthewdalex/sdclang-3.8 prebuilts/clang/linux-x86/host/
+git clone https://bitbucket.org/matthewdalex/sdclang-3.8 prebuilts/clang/linux-x86/host/sdclang-3.8
 git clone https://github.com/Shoaib0597/Velocity_MSM8916 -b N-Tomato-MV kernel/cyanogen/msm8916
